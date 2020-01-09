46681e812ca1bb5c5875693b2e760068590ccfda
Andrey Pogorelov <namtydapro@gmail.com>
2020-01-10 02:17:26 +0300

    test build v5

--- src/index.js
+++ src/index.js
@@ -4,38 +4,49 @@ const { checkHeaders } = require('./headersRules');
 const { checkGridProportions } = require('./gridRules');
 
 let json =
-`[
+  `[
     {
         "block": "text",
-        "mods": { "type": "h3" }
+        "mods": { "type": "h3" },
+        "content": [
+            {
+                "block": "text",
+                "mods": { "type": "h1" }
+            },
+            {
+                "block": "text",
+                "mods": { "type": "h1" }
+            }
+        ]
     },
     {
         "block": "text",
-        "mods": { "type": "h3" }
-    },
-    {
-        "block": "text",
-        "mods": { "type": "h3" }
-    },
-    {
-        "block": "text",
-        "mods": { "type": "h2" }
+        "mods": { "type": "p" },
+        "content": [
+            {
+                "block": "text",
+                "mods": { "type": "h1" }
+            },
+            {
+                "block": "text",
+                "mods": { "type": "h3" }
+            }
+        ]
     }
-    
 ]`;
-
 function lint(str) {
+
   const ast = parse(str);
   const errors = [];
 
-  errors.push(...checkWarning(ast));
-  errors.push(...checkGridProportions(ast));
+  // errors.push(...checkWarning(ast));
+  // errors.push(...checkGridProportions(ast));
   errors.push(...checkHeaders(ast));
 
-  
+
+  // console.dir(errors, { depth: null });
   return errors;
 }
+// lint(json);
 
-
-// globalThis.lint = lint;
-module.exports = lint;
+globalThis.lint = lint;
